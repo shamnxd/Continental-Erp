ae18d2b8a3de60d8da8111d15c151972e9972455
Shamand <shamnadthayyil8@gmail.com>
2026-06-04 18:05:37 +0530

    style(client): update AMC page title to AMC Contracts

--- client/src/features/amc/AMC.tsx
+++ client/src/features/amc/AMC.tsx
@@ -333,7 +333,7 @@ export function AMC() {
   return (
     <>
       <ManagementListPage
-        title="AMC Management"
+        title="AMC Contracts"
         subtitle="Manage annual maintenance contracts"
         headerAction={
           <Button

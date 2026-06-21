83053365f272a23144e4f21cca6be80a0d7774c5
Shamand <shamnadthayyil8@gmail.com>
2026-06-21 13:42:50 +0530

    build(ci): update release action paths and root readme map to root portals

--- client-portals/client-portal/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./index";
-
-// Centralized type-safe wrappers for Redux interaction
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- client-portals/staff-portal/android/app/src/main/java/com/continental/staffportal/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.continental.staffportal;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
